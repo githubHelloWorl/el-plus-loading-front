f4ba15d23f48c9fde6a22f002b39d44c378d95e1
cmd <3417372950@qq.com>
2024-01-27 11:34:11 +0800

    20240127-11:33

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Cascader from "../views/Cascader.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    redirect: "/cascader",
+    component: () => import("../views/Cascader.vue"),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/cascader",
+    name: "cascader",
+    component: () => import("../views/Cascader.vue"),
   },
+  {
+    path: "/loayload",
+    name: "loayload",
+    component: () => import("@/views/LoayLoad.vue"),
+  }
 ];
 
 const router = createRouter({
